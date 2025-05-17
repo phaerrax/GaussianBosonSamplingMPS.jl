6f0f062db25617d22e6f9d8bc10daa26bf8bcb62
Davide Ferracin <davide.ferracin@protonmail.com>
2025-05-17 19:04:27 +0200

    test: tidy up squeezed state checks
    Fix the signs in the coefficients of the squeezed states to align with
    the convention used in GaussianStates, and check the coefficients of the
    MPS vs the direct Franck-Condon formula before normalising the state, so
    that we do not distort the result (even if slightly).

--- GaussianBosonSamplingMPS/test/runtests.jl
+++ GaussianBosonSamplingMPS/test/runtests.jl
@@ -19,6 +19,20 @@ using GaussianBosonSamplingMPS: hafnian, loophafnian, dirsum
     @test loophafnian(dirsum(a, b)) ≈ loophafnian(a) * loophafnian(b)
 end
 
+function squeezed_state_coeff(x, m)
+    if isodd(m)
+        return zero(x)
+    else
+        n = div(m, 2)
+        r, θ = abs(x), angle(x)
+        return (cis(θ) * tanh(r))^n * sqrt(factorial(2n)) /
+               (2^n * factorial(n) * sqrt(cosh(r)))
+    end
+end
+
+squeezed2_state_coeff(r, θ, n) = (cis(θ) * tanh(r))^n / cosh(r)
+squeezed2_state_coeff(x, n) = squeezed2_state_coeff(abs(x), angle(x), n)
+
 @testset verbose = true "Correct construction of MPS of Gaussian states" begin
     @testset "Factorised state has link dims = 1" begin
         # Build an MPS from a Gaussian state that has no entanglement between modes, and
@@ -32,43 +46,55 @@ end
         @test all(linkdims(s) .== 1)
     end
 
-    @testset "Number basis coefficients of squeezed vacuum state" begin
+    @testset "Squeezed vacuum state on a single mode" begin
+        # Build an MPS from a squeezed vacuum state s(r e^(iθ)) and check that
+        # its coefficients in the eigenbasis of the number operator are as expected:
+        #
+        #                    1     +∞                   √(2n)!
+        #   s(r e^(iθ)) = ────────  ∑ (e^(iθ) tanh(r))ⁿ ────── |2n⟩
+        #                 √cosh(r) n=0                   2ⁿn!
+
+        z = rand(ComplexF64)
+        g = vacuumstate(1)
+        squeeze!(g, z, 1)
+        maxn = 10
+
+        coefficients_expected = [squeezed_state_coeff(z, n) for n in 0:maxn]
+
+        _, S = williamson(g.covariance_matrix)
+        Ul, D, Ur = euler(S)
+        coefficients_fc = [
+            GaussianBosonSamplingMPS.franckcondon([n], Ul, D, Ur, [0]) for n in 0:maxn
+        ]
+
+        @test coefficients_expected ≈ coefficients_fc
+    end
+
+    @testset "Squeezed vacuum state on several modes" begin
         # Build an MPS from a product of squeezed vacuum states s(r e^(iθ)) and check that
         # its coefficients in the eigenbasis of the number operator are as expected:
         #
-        #                    1     +∞                    √(2n)!
-        #   s(r e^(iθ)) = ────────  ∑ (-e^(iθ) tanh(r))ⁿ ────── |2n⟩
-        #                 √cosh(r) n=0                    2ⁿn!
+        #                    1     +∞                   √(2n)!
+        #   s(r e^(iθ)) = ────────  ∑ (e^(iθ) tanh(r))ⁿ ────── |2n⟩
+        #                 √cosh(r) n=0                   2ⁿn!
 
         N = 2
-        maxn = 12
+        maxn = 8
         cutoff = 1e-12
-        r = atanh(cutoff^(1/maxn)) * rand(N)
-        θ = 2pi * rand(N)
+        r = atanh(cutoff^(1/maxn)) .* rand(N)
+        θ = 2pi .* rand(N)
         z = r .* cis.(θ)
 
         g = vacuumstate(N)
         squeeze!(g, z)
         v = MPS(g, 4, maxn)  # the MPS will have maxdim=1 anyway
-        normalize!(v)
-        @test all(isone, linkdims(v))
-        @test sum(expect(v, "N")) ≈ number(g)
-
-        function coeff(x, m)
-            if isodd(m)
-                return 0
-            else
-                n = div(m, 2)
-                r, θ = abs(x), angle(x)
-                return (-cis(θ) * tanh(r))^n * sqrt(factorial(2n)) /
-                       (2^n * factorial(n) * sqrt(cosh(r)))
-            end
-        end
+        @test norm(v) ≤ 1 || norm(v) ≈ 1
 
         coefficients_expected = Matrix{ComplexF64}(undef, maxn+1, maxn + 1)
         for n1 in 0:maxn
             for n2 in 0:maxn
-                coefficients_expected[n1 + 1, n2 + 1] = coeff(z[1], n1) * coeff(z[2], n2)
+                coefficients_expected[n1 + 1, n2 + 1] =
+                    squeezed_state_coeff(z[1], n1) * squeezed_state_coeff(z[2], n2)
             end
         end
 
@@ -83,68 +109,76 @@ end
 
         # Another check.
         _, S = williamson(g.covariance_matrix)
+        Ul, D, Ur = euler(S)
         coefficients_fc = similar(coefficients_expected)
         for n1 in 0:maxn
             for n2 in 0:maxn
                 coefficients_fc[n1 + 1, n2 + 1] = GaussianBosonSamplingMPS.franckcondon(
-                    [n1, n2], I(4), S, [0, 0]
+                    [n1, n2], Ul, D, Ur, [0, 0]
                 )
             end
         end
 
         @test coefficients_mps ≈ coefficients_expected ≈ coefficients_fc
+        normalize!(v)
+        @test all(isone, linkdims(v))
+        @test sum(expect(v, "N")) ≈ number(g)
     end
 
-    @testset "Number basis coefficients of two-mode squeezed vacuum state" begin
+    rtol = 1e-6
+    @testset "Two-mode squeezed vacuum state (rtol = $rtol)" begin
         # Build an MPS from a two-mode squeezed vacuum state and check that
         # its coefficients in the eigenbasis of the number operator are as expected:
         #
-        #                  +∞   (-e^(iθ) tanh(r))ⁿ
-        #   s₂(r e^(iθ)) =  ∑  ──────────────────── f(n)⊗f(n)
-        #                  n=0        cosh(r)
+        #                  +∞   (e^(iθ) tanh(r))ⁿ
+        #   s₂(r e^(iθ)) =  ∑  ─────────────────── f(n)⊗f(n)
+        #                  n=0       cosh(r)
 
-        #                ↙ plus or minus here?? (Franck-Condon function says minus...)
-        coeff(r, θ, n) = (-cis(θ) * tanh(r))^n / cosh(r)
-        N = 8
+        maxn = 10
+        maxdim = 10_000
         # The singular values of the reduced state over one of the two modes are
         # (tanh r)^2n / (cosh r)^2, so to get them all in the normal mode decomposition
         # with a cutoff ε = 1e-12 we want an r such that (tanh r)^2n < ε ≤ (cosh r)^2 ε
-        # for all n ≤ N, i.e. r < artanh(ε^(1/2N)).
+        # for all n ≤ maxn, i.e. r < artanh(ε^(1/2maxn)).
         cutoff = 1e-12
-        r = atanh(cutoff^(1/N)) * rand()
+        r = atanh(cutoff^(1/maxn)) * rand()
         θ = 2pi * rand()
         g = vacuumstate(2)
         squeeze2!(g, r*cis(θ), 1, 2)
 
-        v = MPS(g, N^2, N)
+        v = MPS(g, maxdim, maxn)
         sites = siteinds(v)
 
-        coefficients_expected = Diagonal(coeff.(r, θ, 0:N))
+        coefficients_expected = Diagonal(squeezed2_state_coeff.(r, θ, 0:maxn))
         # coefficients_expected[n, m] := ⟨f(n)⊗f(m), s₂(r e^(iθ))⟩ =
         #
-        #                                 (-e^(iθ) tanh(r))ⁿ
-        #                              = ──────────────────── δₘₙ
-        #                                       cosh(r)
+        #                                 (e^(iθ) tanh(r))ⁿ
+        #                              = ─────────────────── δₘₙ
+        #                                      cosh(r)
         #
 
+        # Another check.
+        _, S = williamson(g.covariance_matrix)
+        Ul, D, Ur = euler(S)
+        coefficients_fc = Diagonal([
+            GaussianBosonSamplingMPS.franckcondon([n, n], Ul, D, Ur, [0, 0]) for n in 0:maxn
+        ])
+
+        @test norm(v) ≤ 1 || norm(v) ≈ 1
+        normalize!(v)
+
         coefficients_mps = Matrix{ComplexF64}(undef, size(coefficients_expected))
-        for n in 0:N
-            for m in 0:N
+        for n in 0:maxn
+            for m in 0:maxn
                 coefficients_mps[n + 1, m + 1] = dot(MPS(sites, [string(n), string(m)]), v)
             end
         end
 
-        # Another check.
-        _, S = williamson(g.covariance_matrix)
-        coefficients_fc = Diagonal([
-            GaussianBosonSamplingMPS.franckcondon([n, n], I(4), S, [0, 0]) for n in 0:N
-        ])
-
         # The MPS coefficients here get replaced by zero if their square
         # would be less than the cutoff for the singular values.
-        replace!(x -> abs2(x) < cutoff ? zero(x) : x, coefficients_mps)
         replace!(x -> abs2(x) < cutoff ? zero(x) : x, coefficients_expected)
         replace!(x -> abs2(x) < cutoff ? zero(x) : x, coefficients_fc)
+        replace!(x -> abs2(x) < cutoff ? zero(x) : x, coefficients_mps)
         @test coefficients_mps ≈ coefficients_expected ≈ coefficients_fc
         @test norm(v) ≈ 1
     end
