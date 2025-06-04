57ea42b55ef596743079cb306026e66610ecb93f
Davide Ferracin <davide.ferracin@protonmail.com>
2025-06-04 15:42:04 +0200

    test: measure and sample superboson MPS

--- GaussianBosonSamplingMPS/test/runtests.jl
+++ GaussianBosonSamplingMPS/test/runtests.jl
@@ -443,7 +443,41 @@ const rtol = 1e-6
 
     vv = sb_outer(v)
     #@test firstmoments(vv) ≈ 0 won't work, we can't use `isapprox` with zero
-    @show norm(firstmoments(vv))
     @test norm(firstmoments(vv)) < atol
     @test isapprox(covariancematrix(vv), g.covariance_matrix; rtol=rtol)
+end
+
+@testset verbose=true "Superboson matrix-product states" begin
+    nmodes = 10
+    maxnumber = 5
+    s = siteinds("Boson", nmodes; dim=maxnumber+1)
+
+    @testset "Measurements" begin
+        v = random_mps(s; linkdims=10)
+        vv = sb_outer(v)
+        @test GaussianBosonSamplingMPS.sb_trace(vv) ≈ 1
+        @test expect(v, "n") ≈ measure(vv, [LocalOperator(sb_index(j) => "n") for j in 1:nmodes])
+    end
+
+    @testset "Sampling" begin
+        nsamples = 10
+        ns = rand(0:maxnumber, nmodes)
+        v = MPS(s, string.(ns))
+        orthogonalize!(v, 1)  # required for sampling
+        vv = sb_outer(v)
+        r_std = []
+        r_sb = []
+        for i in nsamples
+            push!(r_std, sample(v))
+            push!(r_sb, sb_sample(vv))
+        end
+        # Since the state is precisely an element of the Fock basis, the sampling procedure
+        # should output the state itself each time. We check that:
+        # · the sampling is identical between the two MPSs
+        @test allequal(r_std) && allequal(r_sb) && all(r_std .== r_sb)
+        # · the sampled basis states are all equal to the one we used to build the MPS at
+        #   the beginning (note that occupation numbers go from `0` to `maxnumber` while MPS
+        #   site indices go from `1` to `maxnumber+1`).
+        @test all(first(r_sb) .== (1 .+ ns))
+    end
 end
