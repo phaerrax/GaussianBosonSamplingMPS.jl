7f318e31e132db389d45890e606c8666aa04779a
Davide Ferracin <davide.ferracin@protonmail.com>
2025-10-15 15:22:40 +0200

    feat: exact displacement operator
    As previously done for the squeezing operator, rewrite the displacement
    operator so that the exact matrix elements are used to build the tensor,
    instead of exponentiating the linear combination of the truncated
    creation and annihilation operator, so that the coefficients do not
    depend on the cutoff.

--- src/mps_operations.jl
+++ src/mps_operations.jl
@@ -213,15 +213,34 @@ end
 
 # Displacement
 
-function ITensors.op(::OpName"displace", st::SiteType"Boson", d::Int; α)
-    return exp(α * op(OpName("a†"), st, d) - conj(α) * op(OpName("a"), st, d))
+function _displace_coefficient(z, n′, n, k, l)
+    # Matrix element ⟨f(n), D(z) f(m)⟩ of the displacement operator
+    #   D(z) = exp(z a* - z̄ a)
+    # on the eigenbasis {f(n)}ₙ of the number operator.
+    return if n′ - k == n - l
+        exp(-0.5abs2(z)) * z^k * (-conj(z))^l / sqrt(factorial(k) * factorial(l)) *
+        sqrt(binomial(n′, k) * binomial(n, l))
+    else
+        zero(z)
+    end
+end
+
+function ITensors.op(::OpName"displace", ::SiteType"Boson", s::Index; α)
+    S = ITensor(ComplexF64, s', s)
+    for n′ in eachval(s'), n in eachval(s)
+        S[s => n, s' => n′] = sum(
+            _displace_coefficient(α, n′-1, n-1, k, l) for k in 0:(n′ - 1), l in 0:(n - 1)
+        )
+    end
+
+    return S
 end
 
 """
-    displace(m::MPS, α; kwargs...)
+    displace(v::Union{MPS, SuperBosonMPS}, α; kwargs...)
 
-Apply a product of single-mode displacement operators on the pure state represented by the
-MPS `m`, with parameter `α[j]` on mode `j`.
+Apply a product of single-mode displacement operators on the pure or mixed state `v` , with
+parameter `α[j]` on mode `j`.
 """
 function GaussianStates.displace(m::MPS, α; kwargs...)
     @assert length(m) == length(α)
@@ -229,12 +248,6 @@ function GaussianStates.displace(m::MPS, α; kwargs...)
     return apply(displacement_ops, m; kwargs...)
 end
 
-"""
-    displace(v::MPS, α; kwargs...)
-
-Apply a product of single-mode displacement operators, with parameter `α[j]` on mode `j`,
-on the mixed state represented by the SuperBosonMPS `v` in the superboson formalism.
-"""
 function GaussianStates.displace(v::SuperBosonMPS, α; kwargs...)
     @assert iseven(length(v))
     nmodes = div(length(v), 2)
